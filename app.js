e2646000a7393f8d775f8c658a3d27b9176cc3a8
piyush35482 <piyushsharma35482@gmail.com>
2024-08-19 19:19:38 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const emoji = document.querySelector("#emoji");
+
+const emojis = [
+  "😆",
+  "😅",
+  "🤣",
+  "😂",
+  "😀",
+  "🤑",
+  "🤨",
+  "🙂",
+  "😊",
+  "😗",
+  "😛",
+  "😏",
+  "🤥",
+  "😴",
+  "🥺",
+  "😧",
+  "😇",
+  "😳",
+  "🙃",
+  "🥴",
+  "🧐",
+  "🤨",
+  "😒",
+  "🤔",
+  "🤭",
+  "🥰",
+  "🤐",
+  "😄",
+  "🤔",
+  "🤪",
+  "🥲",
+  "😃",
+  "😁",
+  "😬",
+];
+
+// emoji.addEventListener("mouseover", () => {
+//   emoji.innerText = emojis[Math.floor(Math.random() * emojis.length)]; // Random 
+// });
+
+
+emoji.addEventListener("mouseover", ()=>{
+  emoji.innerText = emojis[Math.floor(Math.random()*emojis.length)];
+})
